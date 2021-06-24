d51f5a48b538a6e150deec85f60ddd7d6f19fa62
StarPython <1305779886@qq.com>
2021-06-24 15:45:21 +0800

    10

---
+++ 03_《Android移动开发基础案例教程》/补充案例/chapter10/SlidingMenu/app/src/main/java/cn/itcast/slidingmenu/MainActivity.java
@@ -0,0 +1,84 @@
+package cn.itcast.slidingmenu;
+
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.nineoldandroids.view.ViewHelper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initEvents();
+
+    }
+
+    private void initEvents() {
+        mDrawerLayout.addDrawerListener(new DrawerListener() {
+            //改变抽屉变化方法
+            @Override
+            public void onDrawerStateChanged(int newState) {
+            }
+            //抽屉变化时方法
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                View mContent = mDrawerLayout.getChildAt(0);
+                View mMenu = drawerView;
+                float scale = 1 - slideOffset;
+                float rightScale = 0.8f + scale * 0.2f;
+
+                if (drawerView.getTag().equals("LEFT")) {
+
+                    float leftScale = 1 - 0.3f * scale;
+
+                    ViewHelper.setScaleX(mMenu, leftScale);
+                    ViewHelper.setScaleY(mMenu, leftScale);
+                    ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
+                    ViewHelper.setTranslationX(mContent,
+                            mMenu.getMeasuredWidth() * (1 - scale));
+                    ViewHelper.setPivotX(mContent, 0);
+                    ViewHelper.setPivotY(mContent,
+                            mContent.getMeasuredHeight() / 2);
+                    mContent.invalidate();
+                    ViewHelper.setScaleX(mContent, rightScale);
+                    ViewHelper.setScaleY(mContent, rightScale);
+                } else {
+                    ViewHelper.setTranslationX(mContent,
+                            -mMenu.getMeasuredWidth() * slideOffset);
+                    ViewHelper.setPivotX(mContent, mContent.getMeasuredWidth());
+                    ViewHelper.setPivotY(mContent,
+                            mContent.getMeasuredHeight() / 2);
+                    mContent.invalidate();
+                    ViewHelper.setScaleX(mContent, rightScale);
+                    ViewHelper.setScaleY(mContent, rightScale);
+                }
+
+            }
+            //打开抽屉方法
+            @Override
+            public void onDrawerOpened(View drawerView) {
+            }
+            //关闭抽屉方法
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+        });
+    }
+    //初始化控件
+    private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawerLayout);
+    }
+
+}
