e1b8f414a154d27f4d156d5725f570b6823237a5
StarPython <1305779886@qq.com>
2021-06-24 15:43:20 +0800

    123

---
+++ 03_《Android移动开发基础案例教程》/补充案例/chapter04/SaveUserInfo/app/src/main/java/cn/itcast/saveuserinfo/ShowUserInfoActivity.java
@@ -0,0 +1,38 @@
+package cn.itcast.saveuserinfo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowUserInfoActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private TextView tvComp;
+    private TextView tvEmail;
+    private TextView tvPhone;
+    private SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_user_info);
+
+        initView();
+
+        tvName.setText(sp.getString("name", "name"));
+        tvComp.setText(sp.getString("company", "company"));
+        tvEmail.setText(sp.getString("email", "email"));
+        tvPhone.setText(sp.getString("phone", "phone"));
+    }
+
+    private void initView() {
+        tvName = (TextView) findViewById(R.id.tv_name);
+        tvComp = (TextView) findViewById(R.id.tv_company);
+        tvEmail = (TextView) findViewById(R.id.tv_email);
+        tvPhone = (TextView) findViewById(R.id.tv_phone);
+        sp = getSharedPreferences("data", Context.MODE_PRIVATE);
+    }
+
+}
